efa4e3e2203287d8d8bc71e35ff873ee5e0e4e6f
Adrian Morales <adrianmorales80@gmail.com>
2019-05-12 23:19:36 -0500

    fix invalid html issues in console log and added bookmark icon

--- src/components/Works/index.js
+++ src/components/Works/index.js
@@ -2,37 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { withStyles } from '@material-ui/core/styles';
+//import { Grid } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-//import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
-//import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Avatar from '@material-ui/core/Avatar';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
-import { Grid } from '@material-ui/core';
 
 const styles = theme => ({
   root: {
     width: '100%',
     backgroundColor: theme.palette.background.paper,
-    marginLeft: '2rem',
   },
-  align: {
-    display: 'flex',
-    justifyConter: 'center',
-  }
 });
 
 function InsetDividers(props) {
   const { classes } = props;
   return (
     <List className={classes.root}>
-      <ListItem>
+      <ListItem alignItems="flex-start">
+      <ListItemAvatar>
         <Avatar>
           <FontAwesomeIcon icon="book"/>
         </Avatar>
+      </ListItemAvatar>
         <ListItemText 
           primary="The Lion King" 
           secondary={
@@ -40,24 +38,29 @@ function InsetDividers(props) {
               <Typography component="span" className={classes.inline} color="textPrimary">
                 <strong>Description —</strong> But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.
               </Typography>
-              <Grid container style={{justifyContent: "center"}}>
-              <Grid item>
-              <Button size="small" color="primary" className={classes.align}>
+              <span style={{ display: "flex", justifyContent: "center"}}>
+              <Button size="small" color="primary">
                 Read More...
               </Button>
-              </Grid>
-              </Grid>
+              </span>
             </React.Fragment>
           }
           />
-      </ListItem>
+      <ListItemSecondaryAction>
+        <IconButton>
+          <FontAwesomeIcon icon="bookmark"/>
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
       <li>
         <Divider variant="inset" />
       </li>
-      <ListItem>
+      <ListItem alignItems="flex-start">
+      <ListItemAvatar>
         <Avatar>
-        <FontAwesomeIcon icon="scroll"/>
+          <FontAwesomeIcon icon="scroll"/>
         </Avatar>
+      </ListItemAvatar>
         <ListItemText 
           primary="The Little Mermaid" 
           secondary={
@@ -65,22 +68,29 @@ function InsetDividers(props) {
               <Typography component="span" className={classes.inline} color="textPrimary">
                 <strong>Description —</strong> But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.
               </Typography>
-              <Grid container style={{justifyContent: "center"}}>
-              <Grid item>
-              <Button size="small" color="primary" className={classes.align}>
+              <span style={{ display: "flex", justifyContent: "center"}}>
+              <Button size="small" color="primary">
                 Read More...
               </Button>
-              </Grid>
-              </Grid>
+              </span>
             </React.Fragment>
           }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem>
+          />
+      <ListItemSecondaryAction>
+        <IconButton>
+          <FontAwesomeIcon icon="bookmark"/>
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+      <li>
+        <Divider variant="inset" />
+      </li>
+      <ListItem alignItems="flex-start">
+      <ListItemAvatar>
         <Avatar>
-        <FontAwesomeIcon icon="sticky-note"/>
+          <FontAwesomeIcon icon="sticky-note"/>
         </Avatar>
+      </ListItemAvatar>
         <ListItemText 
           primary="Three Little Pigs" 
           secondary={
@@ -88,17 +98,20 @@ function InsetDividers(props) {
               <Typography component="span" className={classes.inline} color="textPrimary">
                 <strong>Description —</strong> But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.
               </Typography>
-              <Grid container style={{justifyContent: "center"}}>
-              <Grid item>
-              <Button size="small" color="primary" className={classes.align}>
+              <span style={{ display: "flex", justifyContent: "center"}}>
+              <Button size="small" color="primary">
                 Read More...
               </Button>
-              </Grid>
-              </Grid>
+              </span>
             </React.Fragment>
           }
-        />
-      </ListItem>
+          />
+      <ListItemSecondaryAction>
+        <IconButton>
+          <FontAwesomeIcon icon="bookmark"/>
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
       <li>
         <Divider variant="inset" />
       </li>
--- src/pages/userProfile.js
+++ src/pages/userProfile.js
@@ -2,14 +2,14 @@ import React from 'react';
 //import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBook, faStickyNote, faScroll } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faStickyNote, faScroll, faBookmark } from '@fortawesome/free-solid-svg-icons';
 import AuthorBanner from '../components/AuthorBanner';
 import ProfileCard from '../components/ProfileCard';
 import CategoryTags from '../components/CategoryTags'
 import Grid from '@material-ui/core/Grid';
 import Works from '../components/Works';
 
-library.add([faBook, faScroll, faStickyNote]);
+library.add([faBook, faScroll, faStickyNote, faBookmark]);
 
 function UserProfile() {
   return (
