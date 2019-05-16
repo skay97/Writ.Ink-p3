e52a9892bc4aca90e3786485347097d790ce99a4
Theo <theobasallaje@gmail.com>
2019-05-16 03:06:34 -0500

    quill working on new posts

---
+++ client/src/components/Quill/Quill.js
@@ -0,0 +1,94 @@
+import React from "react";
+import ReactQuill from "react-quill"
+
+/* 
+ * Simple editor component that takes placeholder text as a prop 
+ */
+class Quill extends React.Component {
+    constructor (props) {
+      super(props)
+      this.state = { editorHtml: '', theme: 'snow' }
+      this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange (html) {
+        this.setState({ editorHtml: html });
+    }
+    
+    handleThemeChange (newTheme) {
+      if (newTheme === "core") newTheme = null;
+      this.setState({ theme: newTheme })
+    }
+    
+    render () {
+      return (
+        <div>
+          <ReactQuill 
+            theme={this.state.theme}
+            onChange={this.handleChange}
+            value={this.state.editorHtml}
+            modules={Quill.modules}
+            formats={Quill.formats}
+            bounds={'.app'}
+            placeholder={this.props.placeholder}
+           />
+          <div className="themeSwitcher">
+            <label>Theme </label>
+            <select onChange={(e) => 
+                this.handleThemeChange(e.target.value)}>
+              <option value="snow">Snow</option>
+              <option value="bubble">Bubble</option>
+              <option value="core">Core</option>
+            </select>
+          </div>
+         </div>
+       )
+    }
+  }
+  
+  /* 
+   * Quill modules to attach to editor
+   * See https://quilljs.com/docs/modules/ for complete options
+   */
+  Quill.modules = {
+    toolbar: [
+      [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+      [{size: []}],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, 
+       {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image', 'video'],
+      ['clean']
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    }
+  }
+  /* 
+   * Quill editor formats
+   * See https://quilljs.com/docs/formats/
+   */
+  Quill.formats = [
+    'header', 'font', 'size',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image', 'video'
+  ]
+  
+  /* 
+   * PropType validation
+   */
+//   Quill.propTypes = {
+//     placeholder
+//   }
+  
+  /* 
+   * Render component on page
+   */
+//   ReactDOM.render(
+//     <Editor placeholder={'Write something...'}/>, 
+//     document.querySelector('.app')
+//   )
+
+export default Quill;
--- client/src/components/blog/post_new.js
+++ client/src/components/blog/post_new.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import { createPost } from '../../actions';
+import Quill from "../Quill";
 
 class PostNew extends Component {
+  constructor(props) {
+    
+    super(props);
+    this.myRef = React.createRef();
+  }
 
   handleFormSubmit({ title, categories, content }) {
-    // console.log(title);
-    // console.log(categories);
-    // console.log(content);
+    console.log('submitting...', this)
+    console.log(title);
+    console.log(categories);
+    console.log(this.myRef.current.state.editorHtml);
+    content = this.myRef.current.state.editorHtml;
     this.props.createPost({ title, categories, content }, (path) => {  // callback 1: history push
       this.props.history.push(path);
     }, (path, state) => {  // callback 2: history replace
       this.props.history.replace(path, state);
     });
+  }
+  handleQuilChange(value) {
+    console.log(value);
+    this.setState({ quilText: value })
   }
 
   renderInput = (field) => (
@@ -24,8 +36,8 @@ class PostNew extends Component {
         {...field.input}
         type={field.type}
         placeholder={field.placeholder}
-        required={field.required? 'required' : ''}
-        disabled={field.disabled? 'disabled' : ''}
+        required={field.required ? 'required' : ''}
+        disabled={field.disabled ? 'disabled' : ''}
       />
     </fieldset>
   );
@@ -33,8 +45,15 @@ class PostNew extends Component {
   renderTextEditor = (field) => (
     <fieldset className="form-group">
       <label>{field.label}</label>
-      <input className="form-control" id="x" type="hidden" name="content" />
-      <trix-editor input="x" {...field.input} />
+      {/* <textarea className="form-control"  {...field.input}  type="text" id="content" name="content" /> */}
+      {/* <input className="form-control" id="x" type="hidden" name="content" /> */}
+      {/* //  input="x" {...field.input} */}
+
+      {/* <trix-editor input="x" {...field.input} /> */}
+      {/* <br></br> */}
+
+      {/* <Quill value={"content"} name="content" placeholder={'Write something...'} /> */}
+      {/* <Quill name="content" value={this.state.text}  onChange={this.handleQuilChange} placeholder={'Write something...'} /> */}
     </fieldset>
   );
 
@@ -45,6 +64,7 @@ class PostNew extends Component {
 
     if (state && action === 'REPLACE') {
       return (
+        // add some animation later from animate.css and remove the state.time for better feedback
         <div className="alert alert-danger" role="alert">
           {`[${state.time}] --- `} <strong>Oops!</strong> {state.message}
         </div>
@@ -63,7 +83,7 @@ class PostNew extends Component {
         <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
           <Field name="title" component={this.renderInput} type="text" label="Title:" placeholder="Enter your title" required={true} />
           <Field name="categories" component={this.renderInput} type="text" label="Categories:" placeholder="Enter your categories, use ',' to separate them" required={true} />
-          <Field name="content" component={this.renderTextEditor} label="Content:" />
+          <Quill ref={this.myRef} name="content" placeholder={'Write something...'} />
           <button action="submit" className="btn btn-primary">Publish</button>
         </form>
       </div>
