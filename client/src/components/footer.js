a179cc583582204eda376de2c16a564680bee2d0
Adrian Morales <adrianmorales80@gmail.com>
2019-05-18 15:17:38 -0500

    merging backend with frontend

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,11 +1,32 @@
+    
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-export default () => {
-  return (
-    <footer className="footer">
-      <div className="container">
-        <span className="text-muted">2019 Writ-Ink</span>
-      </div>
-    </footer>
-  );
+const styles = theme => ({
+    footer: {
+    //   backgroundColor: theme.palette.background.paper,
+      marginTop: theme.spacing.unit * 8,
+      padding: `${theme.spacing.unit * 6}px 0`,
+      backgroundColor: "#424242",
+      color: "#fff"
+    },
+    typo: {
+      color: "white"
+    }
+  });
+
+const Footer = (props) => {
+    const { classes } = props;
+    return (
+    <footer className={classes.footer}>
+        {/* <Typography className={classes.typo} variant="h6" align="center" gutterBottom>
+        </Typography> */}
+        <Typography className={classes.typo} variant="subtitle1" align="center" color="textSecondary" component="p">
+        Content without the noise ©
+        </Typography>
+      </footer>
+    )
 }
+
+export default withStyles(styles)(Footer);
