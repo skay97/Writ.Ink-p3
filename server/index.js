6bb1e803f97c12422b6f070a5bf5382651085aba
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2019-05-18 01:41:11 -0500

    some setup for deployment

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 // const cors = require('cors');  // we don't need it anymore, because we use proxy server instead
 
 // DB Setup (connect mongoose and instance of mongodb)
-mongoose.connect('mongodb://localhost:blog/blog');
+mongoose.connect('mongodb://localhost:writ-ink/writ-ink');
 
 // App Setup (morgan and body-parser are middleware in Express)
 app.use(morgan('combined'));  // middleware for logging
