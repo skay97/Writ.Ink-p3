af4e031f1350af279b8a5427343aa5c65d5600ae
Adrian Morales <adrianmorales80@gmail.com>
2019-05-18 23:36:49 -0500

    updated nav and updated the header

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -9,7 +9,14 @@ const styles = theme => ({
       marginTop: theme.spacing.unit * 8,
       padding: `${theme.spacing.unit * 6}px 0`,
       backgroundColor: "#424242",
-      color: "#fff"
+      color: "#fff",
+      position: "absolute",
+      bottom: 0,
+      width: "100%",
+      /* Set the fixed height of the footer here */
+      height: "60px",
+      lineHeight: "60px" /* Vertically center the text there */
+
     },
     typo: {
       color: "white"
