891980a51003d9255934f0173c2eaecbf450f92e
Adrian Morales <adrianmorales80@gmail.com>
2019-05-21 05:49:49 -0500

    added types that will be needed for profile, posts and comments

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,16 +2,22 @@ export const AUTH_USER = 'auth_user';
 export const UNAUTH_USER = 'unauth_user';
 
 export const FETCH_PROFILE = 'fetch_profile';
+export const FETCH_PROFILES = 'fetch_profiles'; // AM New
 export const CLEAR_PROFILE = 'clear_profile';
 export const UPDATE_PROFILE = 'update_profile';
+export const PROFILE_ERROR = 'profile_error'; // AM New
+export const ACCOUNT_DELETED = 'account_deleted'; // AM New
 
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
 export const FETCH_POST = 'fetch_post';
 export const UPDATE_POST = 'update_post';
 export const DELETE_POST = 'delete_post';
+export const POST_ERROR = 'post_error'; // AM New
+export const UPDATE_LIKES = 'update_likes';  // AM New
 
 export const CHECK_AUTHORITY = 'check_authority';  // check if the user has the authority to make change to a specific post
 
 export const CREATE_COMMENT = 'create_comment';
 export const FETCH_COMMENTS = 'fetch_comments';
+export const REMOVE_COMMENT = 'remove_comment'; // AM New
